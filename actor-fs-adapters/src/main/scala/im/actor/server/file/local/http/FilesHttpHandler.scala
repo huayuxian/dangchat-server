233b642340056184af11f97da6835ebceaa776d1
rdc-justep <song@justep.com>
2016-07-21 19:26:21 +0800

    初始化提交

---
+++ actor-fs-adapters/src/main/scala/im/actor/server/file/local/http/FilesHttpHandler.scala
@@ -0,0 +1,202 @@
+package im.actor.server.file.local.http
+
+import java.time.{ Duration, Instant }
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.model.{ ContentType, HttpResponse, MediaTypes, StatusCodes }
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.headers.ContentDispositionTypes.attachment
+import akka.http.scaladsl.model.headers.{ `Content-Disposition`, `Content-Type` }
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+import im.actor.server.api.http.HttpHandler
+import im.actor.server.api.http.HttpApiHelpers._
+import im.actor.server.file.local.{ FileStorageOperations, LocalFileStorageConfig, RequestSigning }
+import im.actor.util.log.AnyRefLogSource
+
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
+
+private object FilesRejections {
+  case object InvalidFileSignature extends ActorCustomRejection
+}
+
+private[local] final class FilesHttpHandler(storageConfig: LocalFileStorageConfig)(implicit val system: ActorSystem)
+  extends HttpHandler
+  with RequestSigning
+  with FileStorageOperations
+  with AnyRefLogSource {
+  import FilesRejections._
+
+  protected implicit val mat = ActorMaterializer()
+
+  protected implicit val ec: ExecutionContext = system.dispatcher
+  protected val storageLocation = storageConfig.location
+
+  private val log = Logging(system, this)
+
+  val rejectionHandler: RejectionHandler =
+    RejectionHandler.newBuilder()
+      .handle {
+        case InvalidFileSignature ⇒
+          complete(StatusCodes.Forbidden → "Invalid file signature")
+      }
+      .result()
+
+  // format: OFF
+  def routes: Route =
+    extractRequest { request =>
+      //      log.debug("Got file request {}", request)
+      defaultVersion {
+        pathPrefix("files" / SignedLongNumber) { fileId =>
+          options {
+            log.debug("Responded OK to OPTIONS req: {}", request.uri)
+            complete(HttpResponse(OK))
+          } ~
+          validateRequest {
+            get {
+              //we use `Segments` because have to match paths like:
+              //v1/files/:fileId/:fileName
+              //v1/files/:fileId
+              path(Segments(0, 1)) { seqName =>
+                log.debug("Download file request, fileId: {}", fileId)
+                //先得到音频文件名称，如果不是音频文件，则从数据库中得到文件名称
+                val fileName = getVoiceFileName(request.uri.toString())
+                onComplete(getFile(fileId, fileName)) {
+                  case Success(Some(file)) =>
+                    log.debug("Serving fileId: {}, file: {} parts", fileId, file)
+                    //二次开发，如果是音频或视频，则没有Content-Disposition
+                    if(isAudioOrVideo(file.name)) {
+                      respondWithDefaultHeader(
+                        `Content-Type`(getContentType(file.name))
+                      ) {
+                        //TODO: remove as soon, as https://github.com/akka/akka/issues/20338 get fixed
+                        getFromFile(file.toJava)
+                      }
+                    } else {
+                      respondWithDefaultHeader(
+                        //二次开发，对文件名编码，使中文文件名的文件可正确下载  by Lining  2016-6-14
+                        //`Content-Disposition`(attachment, Map("filename" -> file.name))
+                        `Content-Disposition`(attachment, Map("filename" -> java.net.URLEncoder.encode(file.name, "UTF-8")))
+                      ) {
+                        //TODO: remove as soon, as https://github.com/akka/akka/issues/20338 get fixed
+                        getFromFile(file.toJava)
+                      }
+                    }
+                  case Success(None) =>
+                    complete(HttpResponse(StatusCodes.NotFound))
+                  case Failure(e) =>
+                    log.error(e, "Failed to get file content, fileId: {}", fileId)
+                    complete(HttpResponse(500))
+                }
+              }
+            } ~
+            put {
+              pathSuffix(IntNumber) { partNumber =>
+                log.debug("Upload file part request, fileId: {}, partNumber: {}", fileId, partNumber)
+                extractRequest { req =>
+                  val writeFu = for {
+                    _ <- prepareForPartWrite(fileId, partNumber)
+                    _ <- appendPartBytes(req.entity.dataBytes, fileId, partNumber)
+                    _ <- Future {}
+                  } yield ()
+                  onComplete(writeFu) {
+                    case Success(_) =>
+                      log.debug("Successfully uploaded part #{} of file: {} ", partNumber, fileId)
+                      complete(HttpResponse(200))
+                    case Failure(e) =>
+                      log.error(e, "Failed to upload file: {}", fileId)
+                      complete(HttpResponse(500))
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  // format: ON
+
+  /**
+   * 断文件是否是音频或视频 by Lining 2016/7/20
+   *
+   * @param fileName
+   * @return
+   */
+  private def isAudioOrVideo(fileName: String): Boolean = {
+    if (fileName.toLowerCase.endsWith("voice.opus") || fileName.toLowerCase.endsWith("voice.ogg") || fileName.toLowerCase.endsWith("voice.aac")) {
+      true
+    } else if (fileName.toLowerCase.startsWith("vid_") && (fileName.toLowerCase.endsWith(".mp4") || fileName.toLowerCase.endsWith(".mov") || fileName.toLowerCase.endsWith(".ogg"))) {
+      true
+    } else {
+      false
+    }
+  }
+
+  /**
+   * 得到http content type
+   * by Lining 2016/7/20
+   *
+   * @param fileName
+   * @return
+   */
+  private def getContentType(fileName: String): ContentType = {
+    if (fileName.toLowerCase.endsWith(".opus")) {
+      ContentType(MediaTypes.`audio/ogg`)
+    } else if (fileName.toLowerCase.endsWith("voice.ogg")) {
+      ContentType(MediaTypes.`audio/ogg`)
+    } else if (fileName.toLowerCase.endsWith(".aac")) {
+      ContentType(MediaTypes.`audio/vorbis`)
+    } else if (fileName.toLowerCase.startsWith("vid_")) {
+      if (fileName.toLowerCase.endsWith(".mp4")) {
+        ContentType(MediaTypes.`video/mp4`)
+      } else if (fileName.toLowerCase.endsWith(".mov")) {
+        ContentType(MediaTypes.`video/mpeg`)
+      } else {
+        ContentType(MediaTypes.`video/ogg`)
+      }
+    } else {
+      ContentType(MediaTypes.`application/octet-stream`)
+    }
+  }
+
+  /**
+   * 得到音频文件名称
+   *
+   * @param url
+   * @return
+   */
+  private def getVoiceFileName(url: String): String = {
+    if (url.contains("voice.opus")) {
+      "voice.opus"
+    } else if (url.contains("voice.aac")) {
+      "voice.aac"
+    } else if (url.contains("voice.ogg")) {
+      "voice.ogg"
+    } else {
+      ""
+    }
+  }
+
+  def validateRequest: Directive0 =
+    extractRequestContext.flatMap[Unit] { ctx ⇒
+      parameters(("signature", "expires".as[Long])) tflatMap {
+        case (signature, expiresAt) ⇒
+          val request = ctx.request
+          val uriWithoutSignature = request.uri.withQuery(request.uri.query() filterNot { case (k, _) ⇒ k == "signature" })
+          val notExpired = isNotExpired(expiresAt)
+          val calculatedSignature = calculateSignature(request.method, uriWithoutSignature)
+          //去掉用户签名校验    by Lining   2016-6-16
+          /*if (notExpired && calculatedSignature == signature) pass else {
+            log.debug("Failed to validate request: {}, notExpired: {}, signature: {}; calculated signature: {}", notExpired, ctx.request, signature, calculatedSignature)
+            reject(InvalidFileSignature)
+          }*/
+          pass
+      }
+    }
+
+  private def isNotExpired(expiresAt: Long) = expiresAt <= Instant.now.plus(Duration.ofDays(1)).toEpochMilli
+
+}
---
+++ actor-models/src/main/scala/im/actor/server/model/Group.scala
@@ -0,0 +1,49 @@
+package im.actor.server.model
+
+import java.time.Instant
+
+case class Group(
+  id:            Int,
+  creatorUserId: Int,
+  accessHash:    Long,
+  title:         String,
+  isPublic:      Boolean,
+  createdAt:     Instant,
+  about:         Option[String],
+  topic:         Option[String],
+  isShare:       Boolean
+)
+
+object Group {
+  def fromFull(fullGroup: FullGroup): Group =
+    Group(
+      id = fullGroup.id,
+      creatorUserId = fullGroup.creatorUserId,
+      accessHash = fullGroup.accessHash,
+      title = fullGroup.title,
+      isPublic = fullGroup.isPublic,
+      createdAt = fullGroup.createdAt,
+      about = fullGroup.about,
+      topic = fullGroup.topic,
+      isShare = fullGroup.isShare
+    )
+}
+
+case class FullGroup(
+  id:                   Int,
+  creatorUserId:        Int,
+  accessHash:           Long,
+  title:                String,
+  isPublic:             Boolean,
+  createdAt:            Instant,
+  about:                Option[String],
+  topic:                Option[String],
+  titleChangerUserId:   Int,
+  titleChangedAt:       Instant,
+  titleChangeRandomId:  Long,
+  avatarChangerUserId:  Int,
+  avatarChangedAt:      Instant,
+  avatarChangeRandomId: Long,
+  isHidden:             Boolean,
+  isShare:              Boolean
+)
